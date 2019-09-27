2f9010580ad1a76bcfd7305796c7def9af26eedf
nopparat231 <23.noop@gmail.com>
2019-09-27 21:23:28 +0700

    fix db name

--- Connections/condb.php
+++ Connections/condb.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(0);
 $hostname_condb = "localhost";
-$database_condb = "leather-shop";
+$database_condb = "t_shirt_shop";
 $username_condb = "root";
 $password_condb = "";
 $condb = mysql_connect($hostname_condb, $username_condb, $password_condb , $database_condb) or trigger_error(mysql_error(),E_USER_ERROR);
--- db_connect.php
+++ db_connect.php
@@ -3,6 +3,6 @@ error_reporting(0);
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "leather-shop";
+$dbname = "t_shirt_shop";
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 ?>
