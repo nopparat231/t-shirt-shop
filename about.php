ebf1a24caca184d65addedc34d01624c921b28ae
moroongnapa <55760117+moroongnapa@users.noreply.github.com>
2019-09-29 13:59:50 +0700

    โลโก้

--- about.php
+++ about.php
@@ -5,8 +5,8 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="section__title section__title--2 text-center">
-                            <h2 class="title__line">Welcome To Uniqlo Store</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore gna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.</p>
+                            <h2 class="title__line">ยินดีต้อนรับสู่ ร้านUp2screen</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore gScreenna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.</p>
                         </div>
                         <div class="store__btn">
                             <a href="#">contact us</a>
