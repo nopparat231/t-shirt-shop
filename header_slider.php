21f1a17d9270100e531ab737fa9677e3a3c7f344
moroongnapa <55760117+moroongnapa@users.noreply.github.com>
2019-10-12 20:12:17 +0700

    เปลี่ยนชื่อสินค้า เปลี่ยนรูป
    เปลี่ยนชื่อสินค้า เปลี่ยนรูป

--- header_slider.php
+++ header_slider.php
@@ -2,7 +2,7 @@
     <div class="slider__container slider--one">
         <div class="slider__activation__wrap owl-carousel owl-theme">
             <!-- Start Single Slide -->
-            <div class="slide slider__full--screen" style="background: rgba(0, 0, 0, 0) url() no-repeat scroll center center / cover ;">
+            <div class="slide slider__full--screen" style="background: rgba(0, 0, 0, 0) url(images/slider/bg/1800x800.jpg) no-repeat scroll center center / cover ;">
                 <div class="container">
                     <div class="row">
                         <div class="col-md-8 col-lg-8 col-md-offset-2 col-lg-offset-4 col-sm-12 col-xs-12">
@@ -18,7 +18,7 @@
             </div>
             <!-- End Single Slide -->
             <!-- Start Single Slide -->
-            <div class="slide slider__full--screen" style="background: rgba(0, 0, 0, 0) url() no-repeat scroll center center / cover ;">
+            <div class="slide slider__full--screen" style="background: rgba(0, 0, 0, 0) url(images/slider/bg/1800x800.jpg) no-repeat scroll center center / cover ;">
                 <div class="container">
                     <div class="row">
                         <div class="col-md-8 col-lg-8 col-sm-12 col-xs-12">
